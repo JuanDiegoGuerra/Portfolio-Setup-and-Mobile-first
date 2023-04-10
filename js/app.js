4588391c2fd71d108427da9e69dc0b2a9d2e5c3d
CesarHerr <cesarherr321@gmail.com>
2023-04-10 18:43:36 -0400

    Update app.js
    Added event listeners

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 const hamburger = document.querySelector(".menu");
 const navMenu = document.querySelector(".flex-nav-list");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+
+navMenu.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
