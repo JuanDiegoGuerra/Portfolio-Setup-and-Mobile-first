3eb8a437598e7035ef9cbc89d1da457bf1be79cb
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-10 22:13:02 -0500

    Update app.js
    Fixed Linter

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector(".menu");
-const navMenu = document.querySelector(".flex-nav-list");
+const hamburger = document.querySelector('.menu');
+const navMenu = document.querySelector('.flex-nav-list');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  })
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-navMenu.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  })
+navMenu.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
