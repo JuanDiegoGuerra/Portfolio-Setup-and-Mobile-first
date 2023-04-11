b179c208ddb143b746c7f52f1672a37e10b2b37b
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-11 17:26:18 -0500

    Update app.js
    Create Object/Constructor for Project Cards
    Added work 1 / project 1

--- js/app.js
+++ js/app.js
@@ -10,3 +10,19 @@ navMenu.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
+
+const ProjectCardsDetails = [
+  {
+    id: 'work1',
+    projectName: 'Hello-Microverse',
+    projectInfo:
+      "Hello-Microverse-Project is mean to be an example of how to set up a simple project using git & github best practices.",
+    ProjectIMG: [
+      'img/project_imgs/hello_microverse.JPG'
+    ],
+    languages: ['Git', 'Github', 'CSS', 'HTML5'],
+    seeProject: 'See Project',
+    SeeLive: 'https://github.com/JuanDiegoGuerra/Hello-Microverse-Project',
+    SeeSource: 'https://github.com/JuanDiegoGuerra/Hello-Microverse-Project',
+  }        
+]   
