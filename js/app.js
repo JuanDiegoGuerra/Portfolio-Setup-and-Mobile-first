673b4427f08d15038ec6d5365ac0bf1ae96c2a14
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-12 03:27:40 -0500

    Update app.js
    Fixed Linter (Eslint JS) Erros

--- js/app.js
+++ js/app.js
@@ -16,9 +16,9 @@ const ProjectCardsDetails = [
     id: 'work1',
     projectName: 'Hello-Microverse',
     projectInfo:
-      "Hello-Microverse-Project is mean to be an example of how to set up a simple project using git & github best practices.",
+      'Hello-Microverse-Project is mean to be an example of how to set up a simple project using git & github best practices.',
     ProjectIMG: [
-      'img/project_imgs/hello_microverse.JPG'
+      'img/project_imgs/hello_microverse.JPG',
     ],
     languages: ['Git', 'Github', 'CSS', 'HTML5'],
     seeProject: 'See Project',
@@ -29,9 +29,9 @@ const ProjectCardsDetails = [
     id: 'work2',
     projectName: 'Setup-Mobile-First',
     projectInfo:
-      "Setup-Mobile-First is a project in where you built your own portfolio website using Frontend Dev Tools.",
+      'Setup-Mobile-First is a project in where you built your own portfolio website using Frontend Dev Tools.',
     ProjectIMG: [
-      'img/project_imgs/setup_portfolio.JPG'
+      'img/project_imgs/setup_portfolio.JPG',
     ],
     languages: ['HTML5', 'Javascript', 'CSS', 'Github'],
     seeProject: 'See Project',
@@ -42,9 +42,9 @@ const ProjectCardsDetails = [
     id: 'work3',
     projectName: 'Pokedex',
     projectInfo:
-      "Pokedex is a personal project that I made watching a video and practicing what I learnd in my Web Developer journey.",
+      'Pokedex is a personal project that I made watching a video and practicing what I learnd in my Web Developer journey.',
     ProjectIMG: [
-      'img/project_imgs/pokedex_preview2.gif'
+      'img/project_imgs/pokedex_preview2.gif',
     ],
     languages: ['Javascript', 'HTML5', 'CSS', 'Github'],
     seeProject: 'See Project',
@@ -55,9 +55,9 @@ const ProjectCardsDetails = [
     id: 'work4',
     projectName: 'Caesar Cipher',
     projectInfo:
-      "Caesar Cipher is a personal project that I made watching a video and practicing what I learnd in my Web Developer journey.",
+      'Caesar Cipher is a personal project that I made watching a video and practicing what I learnd in my Web Developer journey.',
     ProjectIMG: [
-      'img/project_imgs/caesar_cipher_preview.gif'
+      'img/project_imgs/caesar_cipher_preview.gif',
     ],
     languages: ['CSS', 'HTML5', 'Github', 'Javascript'],
     seeProject: 'See Project',
@@ -68,9 +68,9 @@ const ProjectCardsDetails = [
     id: 'work5',
     projectName: 'Chronometer',
     projectInfo:
-      "Chronometer is a personal project that I made watching a video and practicing what I learnd in my Web Developer journey.",
+      'Chronometer is a personal project that I made watching a video and practicing what I learnd in my Web Developer journey.',
     ProjectIMG: [
-      'img/project_imgs/chronometer_preview.gif'
+      'img/project_imgs/chronometer_preview.gif',
     ],
     languages: ['HTML5', 'Javascript', 'CSS', 'Github'],
     seeProject: 'See Project',
@@ -81,16 +81,16 @@ const ProjectCardsDetails = [
     id: 'work6',
     projectName: 'Try Boostrap',
     projectInfo:
-      "Try Boostrap is a project in where you built your own portfolio website using Boostrap.",
+      'Try Boostrap is a project in where you built your own portfolio website using Boostrap.',
     ProjectIMG: [
-      'img/project_imgs/try_boostrap.JPG'
+      'img/project_imgs/try_boostrap.JPG',
     ],
     languages: ['Boostrap', 'HTML5', 'CSS', 'Github'],
     seeProject: 'See Project',
     SeeLive: 'https://github.com/JuanDiegoGuerra/Portfolio-Setup-and-Mobile-first',
     SeeSource: 'https://github.com/JuanDiegoGuerra/Portfolio-Setup-and-Mobile-first',
-  }        
-]   
+  },
+];
 // Pop Up section
 const PopupWindow = document.querySelectorAll('.external_link');
 const popUp = document.querySelector('.popup');
@@ -126,9 +126,10 @@ PopupWindow.forEach((p, index) => {
           <i class="fa fa-github" aria-hidden="true"></i></a> </button>
         </div>
       </div>`;
-      const closePop = document.querySelector('.close-pop');
+    const closePop = document.querySelector('.close-pop');
     closePop.addEventListener('click', () => {
       popUp.classList.remove('display-visible');
       document.body.classList.remove('no-scroll');
-
-    }); }); });
+    });
+  });
+});
