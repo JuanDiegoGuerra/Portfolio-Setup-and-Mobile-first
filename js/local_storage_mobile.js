debaf1c2cd781b982ee9121ec8f39ad235b642f3
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-13 20:00:47 -0500

    Update local_storage_mobile.js
    Fixed linters

--- js/local_storage_mobile.js
+++ js/local_storage_mobile.js
@@ -2,23 +2,23 @@ const namesM = document.getElementById('fullname');
 const emailM = document.getElementById('emailMobile');
 const messageM = document.getElementById('txtSMobile');
 const dataM = localStorage.getItem('formDatamo');
-const storageDataM = JSON.parse(dataM)
+const storageDataM = JSON.parse(dataM);
 
 if (storageDataM) {
-    namesM.value = storageDataM.fullname;
-    emailM.value = storageDataM.emailMobile;
-    messageM.value = storageDataM.txtSMobile;
+  namesM.value = storageDataM.fullname;
+  emailM.value = storageDataM.emailMobile;
+  messageM.value = storageDataM.txtSMobile;
 } else {
-    namesM.value = "";
-    emailM.value = "";
-    messageM.value = "";
+  namesM.value = '';
+  emailM.value = '';
+  messageM.value = '';
 }
 
 formValidatorM.addEventListener('input', () => {
-    const formDataM = {
-        fullname: namesM.value,
-        emailMobile: emailM.value,
-        txtSMobile: messageM.value,
-    };
-    localStorage.setItem('formDatamo', JSON.stringify(formDataM));
+  const formDataM = {
+    fullname: namesM.value,
+    emailMobile: emailM.value,
+    txtSMobile: messageM.value,
+  };
+  localStorage.setItem('formDatamo', JSON.stringify(formDataM));
 });
