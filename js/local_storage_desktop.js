498a9e97e6472564b7ce187e5025dc3b931df077
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-13 19:59:56 -0500

    Update local_storage_desktop.js
    Fixed linters

--- js/local_storage_desktop.js
+++ js/local_storage_desktop.js
@@ -3,28 +3,26 @@ const lname = document.getElementById('lastname');
 const emailD = document.getElementById('emailDesktop');
 const messageD = document.getElementById('txtSDesktop');
 const dataD = localStorage.getItem('formData');
-const storageDataD = JSON.parse(dataD)
+const storageDataD = JSON.parse(dataD);
 
 if (storageDataD) {
-    fname.value = storageDataD.firstname;
-    lname.value = storageDataD.lastname;
-    emailD.value = storageDataD.emailDesktop;
-    messageD.value = storageDataD.txtSDesktop;
+  fname.value = storageDataD.firstname;
+  lname.value = storageDataD.lastname;
+  emailD.value = storageDataD.emailDesktop;
+  messageD.value = storageDataD.txtSDesktop;
 } else {
-    fname.value = "";
-    lname.value = "";
-    emailD.value = "";
-    messageD.value = "";
+  fname.value = '';
+  lname.value = '';
+  emailD.value = '';
+  messageD.value = '';
 }
 
 formValidatorD.addEventListener('input', () => {
-    const formDataD = {
-        firstname: fname.value,
-        lastname: lname.value,
-        emailDesktop: emailD.value,
-        txtSDesktop: messageD.value,
-    };
-    localStorage.setItem('formData', JSON.stringify(formDataD));
+  const formDataD = {
+    firstname: fname.value,
+    lastname: lname.value,
+    emailDesktop: emailD.value,
+    txtSDesktop: messageD.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formDataD));
 });
-
-
