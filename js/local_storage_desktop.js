f01384a12f3e8be1e2a6ec45f74e4c9da547ec51
CesarHerr <cesarherr321@gmail.com>
2023-04-13 20:48:38 -0400

    Update local_storage_desktop.js
    Added local storage function event on the desktop version

--- js/local_storage_desktop.js
+++ js/local_storage_desktop.js
@@ -2,4 +2,29 @@ const fname = document.getElementById('firstname');
 const lname = document.getElementById('lastname');
 const emailD = document.getElementById('emailDesktop');
 const messageD = document.getElementById('txtSDesktop');
+const dataD = localStorage.getItem('formData');
+const storageDataD = JSON.parse(dataD)
+
+if (storageDataD) {
+    fname.value = storageDataD.firstname;
+    lname.value = storageDataD.lastname;
+    emailD.value = storageDataD.emailDesktop;
+    messageD.value = storageDataD.txtSDesktop;
+} else {
+    fname.value = "";
+    lname.value = "";
+    emailD.value = "";
+    messageD.value = "";
+}
+
+formValidatorD.addEventListener('input', () => {
+    const formDataD = {
+        firstname: fname.value,
+        lastname: lname.value,
+        emailDesktop: emailD.value,
+        txtSDesktop: messageD.value,
+    };
+    localStorage.setItem('formData', JSON.stringify(formDataD));
+});
+
 
